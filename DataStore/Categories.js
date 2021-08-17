5a28c9c735530204bd5a090acf3b6717143cb1e7
Aditya <88943335+aditya-js@users.noreply.github.com>
2021-08-17 12:39:40 +0530

    Cat, products json

---
+++ DataStore/Categories.js
@@ -0,0 +1,10 @@
+export const categories = [
+  {
+    categoryId: 1,
+    categoryName: "Lamp"
+  },
+  {
+    categoryId: 2,
+    categoryName: "Chair"
+  }
+];
---
+++ DataStore/Products.js
@@ -0,0 +1,8 @@
+export const products = [
+  {
+    productId: 1,
+    SKU: "fancy_Lamp_Yellow",
+    productName: "Fancy Lamp",
+    varient: [{}]
+  }
+];
